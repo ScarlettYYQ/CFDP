9c463c975d559af7f514661fcab12975a29a30f0
Scarlett <yangyaqi0423@gmail.com>
2025-12-04 14:26:37 +1100

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python CFDP/generate_cot_wrong.py --key REMOVED --base_url REMOVED
+python CFDP/generate_cot_wrong.py --key YOURKEY --base_url URL
 python CFDP/generate_input.py
-python CFDP/demo.py --key REMOVED --base_url REMOVED --type ambig
+python CFDP/demo.py --key YOURKEY --base_url URL --type ambig
